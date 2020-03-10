aebf97dd2fa2c4760755ea2cf2c37fc171a9c2b2
yaoyuanyy <yaoliang_yl_ok@163.com>
2020-03-10 12:36:26 +0800

    java agent入门例子

--- microservice-comb-agent/src/main/java/com/skyler/cobweb/agent/AgentTest.java
+++ microservice-comb-agent/src/main/java/com/skyler/cobweb/agent/AgentTest.java
@@ -7,6 +7,10 @@ package com.skyler.cobweb.agent;
  *
  *   NB. jvm需要加参数
  *   VM options: -javaagent:/Users/yaoliang/skyler/project/lianjia/microservice-comb/microservice-comb-agent/target/microservice-comb-agent-1.0.0-SNAPSHOT.jar=hello
+ *
+ *   参考：
+ *   https://blog.csdn.net/xifeijian/article/details/79991913
+ *   https://blog.csdn.net/wangzhongshun/article/details/100287986
  * </pre>
  * <p>
  * Created by skyler on 2020/3/9 at 5:50 PM
